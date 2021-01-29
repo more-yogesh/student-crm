40e01d3338b74af74c1be0241c53b0017410a646
shrikhaKhalasi <khalasishrikha@gmail.com>
2021-01-29 13:17:57 +0530

    validation done

---
+++ app/Http/Requests/DealRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class DealRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'user_id' => 'required',
+            'lead_id' => 'required',
+            'amount' => 'required|numeric|max:6',
+            'description' => 'required|max:200',
+            'status' => 'required'
+        ];
+    }
+}
